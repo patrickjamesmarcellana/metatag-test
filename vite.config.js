f25dbaa350b0abca11195bea79ac364ce79b6557
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-07-20 17:23:17 +0800

    fix: Another fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ command, ssrBuild }) => {
   return {
     plugins: [react()],
     build: {
       outDir: ssrBuild ? 'dist/server' : 'dist/client',
-      ssr: ssrBuild ? './server/entry-server.jsx' : false,
-      rollupOptions: ssrBuild
-        ? { input: './server/entry-server.jsx' }
-        : undefined,
+      ssr: ssrBuild ? 'server/entry-server.jsx' : false,
     },
     ssr: {
-      noExternal: ['react-helmet'], // important for SSR
+      noExternal: ['react-helmet-async'], // ensure proper hydration
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
   }
 })
