f6f5866851a71a51821def847ce4499f9ad20395
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2025-07-20 17:16:43 +0800

    fix: Modify scripts and config files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, ssrBuild }) => {
+  if (ssrBuild) {
+    // SSR build config
+    return {
+      plugins: [react()],
+      build: {
+        ssr: true,
+        outDir: 'dist/server',
+        rollupOptions: {
+          input: './server/entry-server.jsx',
+        },
+      },
+      ssr: {
+        noExternal: ['react-helmet-async'],
+      },
+    }
+  } else {
+    // Client build config
+    return {
+      plugins: [react()],
+      build: {
+        outDir: 'dist/client', // prevent overwriting SSR
+      },
+    }
+  }
 })
